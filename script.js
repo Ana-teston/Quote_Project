72b54ab112d3f191be88b38b7dffc2d8c0ac30e6
Ana-teston <anapaulateston@gmail.com>
2022-07-15 16:25:08 +0200

    Loader

--- script.js
+++ script.js
@@ -3,8 +3,21 @@ const quoteText = document.getElementById('quote');
 const authortext = document.getElementById('author');
 const twitterBtn = document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
 
 let apiQuotes = [];
+
+// Show loader
+function loading() {
+  loader.hidden = false;
+  quoteContainer.hidden = true;
+}
+
+//Hide loading
+function complete() {
+  quoteContainer.hidden = false;
+  loader.hidden = true;
+}
 
 // Show new quote
 function newQuote() {
@@ -24,6 +37,10 @@ function newQuote() {
   }
   quoteText.textContent = quote.text;
 }
+
+// Set quote, hide loader
+quoteText.textContent = quote.text;
+complete();
 
 // Get quotes from API
 async function getQuotes() {
